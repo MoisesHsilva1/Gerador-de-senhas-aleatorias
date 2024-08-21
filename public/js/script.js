31114dfef40a50298161be374d2eb5bd0c0fb2fd
Moisés Henrique <142702205+MoisesHsilva1@users.noreply.github.com>
2024-08-21 20:12:56 -0300

    feat: implementando funcao de gerar com letras maiuculas

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,41 @@
 class GeratePassword {
-    constructor () {
-        this.lowercaseLetters = document.getElementById('checkbox-lowercaseLetters'); 
+    constructor() {
+        this.lowercaserLetters = document.getElementById('checkbox-lowercaseLetters');
         this.resulRandomPassword = document.getElementById('random-password');
+        this.upcaserLetters = document.getElementById('checkbox-UpcaseLetters');
+        this.numberCharacters = document.getElementById('input-numberCharacters');
+        this.string = ["abcdefghijklmnopqstuvxwyz"];
+    }
+    fieldSetNumberCharactersPassword() {
+        var valueField = '12'; 
+        var notvalueField = '';
+        (notvalueField === '' || isNaN(Number(valueField)))
+            ? window.alert('Por favor, preencha o campo com um número') 
+            : null;
     }
     geratePasswordLowCaser() {
-        if(this.lowercaseLetters.checked = true) {
-            const lowecaser = "abcdefghijklmnopqstuvxwyz"
-            const results = lowecaser[Math.floor(Math.random()) * lowecaser.length]
-            this.resulRandomPassword.textContent = results
-        } 
+        if (this.lowercaserLetters.checked == true) {
+            const lowecaser = this.string[0].toLowerCase();
+            const randomLowerCaser = lowecaser[Math.floor(Math.random()) * lowecaser.length]
+            this.resulRandomPassword.textContent = randomLowerCaser
+            return true;
+        }
+        return false;
     }
-}      
+    geratePasswordUpCase() {
+        if (this.upcaserLetters.checked == true) {
+            const upcaser = this.string[0].toUpperCase();
+            const randomUpcaser = upcaser[Math.floor(Math.random()) * upcaser.length]
+            this.resulRandomPassword.textContent = randomUpcaser
+            return true;
+        }
+        return false;
+    }
+}
 const geratePassword = new GeratePassword();
 
-document.getElementById('button-gerate-password').addEventListener('click', function() {
+document.getElementById('button-gerate-password').addEventListener('click', function () {
     geratePassword.geratePasswordLowCaser();
+    geratePassword.geratePasswordUpCase();
+    geratePassword.fieldSetNumberCharactersPassword();
 })
