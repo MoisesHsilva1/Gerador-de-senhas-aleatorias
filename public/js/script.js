6e6ed9fa60693f2a5690088e59be2d2cd84c6be8
Moisés Henrique <142702205+MoisesHsilva1@users.noreply.github.com>
2024-08-26 20:33:09 -0300

    refactor: melhorando a logica e a sintaxe do codigo

--- public/js/script.js
+++ public/js/script.js
@@ -4,51 +4,60 @@ class GeratePassword {
         this.resulRandomPassword = document.getElementById('random-password');
         this.upeerCaserLetters = document.getElementById('checkbox-UpcaseLetters');
         this.numbersPassword = document.getElementById('checkbox-numbers');
-        this.numberCharacters = document.getElementById('input-numberCharacters');
-        this.specialCharacters = document.getElementById('checkbox-specialCharacters')
-        this.passwords = ["abcdefghijklmnopqstuvxwyz", "1234567891012345678", "!@#$%&*()?!@@##4"];
-    }
-    fieldSetNumberCharactersPassword() {
-        this.numberCharacters.value === '' && window.alert('Por favor, preencha o campo com um número')
+        this.numberCharacters = document.getElementById('range-numberCharacters');
+        this.specialCharacters = document.getElementById('checkbox-specialCharacters');
+        this.spanNumbersRange = document.getElementById('numberCharactersPassword');
+        this.passwords = [
+            "abcdefghijklmnopqstuvxwyz",
+            "1234567891012345678",
+            "!@#$%&*()?!@@##4"
+        ];
     }
     geratePasswordLowCaser() {
-        if (this.lowerCaserLetters.checked == true) {
-            const lowecaser = this.passwords[0].toLowerCase();
-            const randomLowerCaser = lowecaser[Math.floor(Math.random()) * lowecaser.length]
+        if (this.lowerCaserLetters.checked) {
+            const lowecaser = this.passwords[0]
+            const randomLowerCaser = lowecaser[Math.floor(Math.random() * lowecaser.length)]
             this.resulRandomPassword.textContent = randomLowerCaser
-            return true;
         }
-        return false;
+        return '';
+
     }
     geratePasswordUpeerCase() {
-        if (this.upeerCaserLetters.checked == true) {
+        if (this.upeerCaserLetters.checked) {
             const upcaser = this.passwords[0].toUpperCase();
-            const randomUpcaser = upcaser[Math.floor(Math.random()) * upcaser.length]
+            const randomUpcaser = upcaser[Math.floor(Math.random() * upcaser.length)]
             this.resulRandomPassword.textContent = randomUpcaser
-            return true;
         }
-        return false;
+        return '';
     }
     geratePasswordNumbers() {
-        if (this.numbersPassword.checked == true) {
+        if (this.numbersPassword.checked) {
             const numbers = this.passwords[1]
-            const randomNumbers = numbers[Math.floor(Math.random()) * numbers.length]
+            const randomNumbers = numbers[Math.floor(Math.random() * numbers.length)]
             this.resulRandomPassword.textContent = randomNumbers
         }
+        return '';
     }
     gerateSpecialCharacters() {
         if (this.specialCharacters.checked == true) {
             const specialCharacters = this.passwords[2]
-            const randomSpecialCharacters = specialCharacters[Math.floor(Math.random()) * specialCharacters.length]
+            const randomSpecialCharacters = specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
             this.resulRandomPassword.textContent = randomSpecialCharacters
         }
+        return '';
+    }
+    gerateNumerbsRange() {
+        this.numberCharacters.max < this.numberCharacters.min || this.numberCharacters.min < this.numberCharacters.max
+            ? this.spanNumbersRange.textContent = 10
+            : this.spanNumbersRange.textContent = 20
+
     }
     addEventsListers() {
         this.geratePasswordLowCaser();
         this.geratePasswordUpeerCase();
-        this.fieldSetNumberCharactersPassword();
         this.geratePasswordNumbers();
         this.gerateSpecialCharacters();
+        this.gerateNumerbsRange();
     }
 }
 const geratePassword = new GeratePassword();
